eaed2081b6905a7e7c8184f5b6d386800a222637
Gayla Cloud <gaylacloud@gaylas-mbp.home>
2020-10-12 18:16:50 -0500

    Updated config.json to config.js for security

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
+module.exports=
 {
   "development": {
     "username": "root",
-    "password": null,
+    "password": process.env.dbpass,
     "database": "meally_app",
     "host": "127.0.0.1",
     "dialect": "mysql"
